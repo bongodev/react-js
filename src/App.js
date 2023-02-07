f529bbf988658cc141d3f02d820ccabc5bf991b1
kmtusher97 <kmtusher97@gmail.com>
2023-02-07 21:50:57 +0600

    extract component and use it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
-function App() {
+function Student() {
   return (
-    <div className="App">
+    <div>
       <h1>Name: Kamrul Hasan</h1>
       <h2>Institute: Jahangirnagar University</h2>
       <h3>Department: CSE</h3>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Student></Student>
     </div>
   );
 }
