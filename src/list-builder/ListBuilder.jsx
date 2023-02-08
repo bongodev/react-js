e4e667dc8fcab2fedc879da7686cf179530cf846
kmtusher97 <kmtusher97@gmail.com>
2023-02-08 21:47:57 +0600

    handle adding item to the list with useState

--- src/list-builder/ListBuilder.jsx
+++ src/list-builder/ListBuilder.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 function ListBuilder() {
-  const list = ['Apple', 'Dell', 'HP', 'ASUS', 'ACER', 'Samsung', 'Walton'];
+  const [newItem, setNewItem] = useState('');
+  const [list, setList] = useState([]);
+
+  function handleOnChange(event) {
+    setNewItem(event.target.value);
+  }
+
+  function handleAddItem() {
+    if (!newItem) {
+      return;
+    }
+    setList([...list, newItem]);
+  }
 
   return (
     <div className="ListBuilder">
       <div className="Input">
-        <input />
-        <button>Add</button>
+        <input onChange={handleOnChange} />
+        <button onClick={handleAddItem}>Add</button>
       </div>
       <div className="List">
         <ul>
