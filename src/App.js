6be2e72f07d736ef4a41844a5cd5f31717d3743e
kmtusher97 <kmtusher97@gmail.com>
2023-02-08 21:05:40 +0600

    add Counter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+function Counter() {
+  let count = 0;
+
+  const handleIncrease = () => {
+    count++;
+    console.log(count);
+  };
+
+  return (
+    <div className="Counter">
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={handleIncrease}>+</button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      React Examples
+      <Counter />
     </div>
   );
 }
