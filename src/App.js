77d796070e536d1599d5fd4bf388d8948934b99f
kmtusher97 <kmtusher97@gmail.com>
2023-02-08 21:14:58 +0600

    move Counter component in a separate file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import { useState } from 'react';
+import Counter from './Counter';
+
 import './App.css';
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrease = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <div className="Counter">
-      <h1>Counter</h1>
-      <p>{count}</p>
-      <button onClick={handleIncrease}>+</button>
-      <button onClick={handleDecrease}>-</button>
-    </div>
-  );
-}
 
 function App() {
   return (
---
+++ src/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="Counter">
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={handleIncrease}>+</button>
+      <button onClick={handleDecrease}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
