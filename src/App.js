ecaf7ef6ecca6bc8d2840322b8a8fea48869381b
kmtusher97 <kmtusher97@gmail.com>
2023-02-08 21:12:18 +0600

    add decrement button

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   const handleIncrease = () => {
-    console.log('updating state');
     setCount(count + 1);
-    console.log('state update completed, but not updated immidiately', count);
-    /**
-     * the state count will be updated eventually
-     * but as soon as the state count gets updated
-     * the component will be re-rendered
-     */
+  };
+
+  const handleDecrease = () => {
+    setCount(count - 1);
   };
 
   return (
     <div className="Counter">
-      {console.log('rendering....')}
       <h1>Counter</h1>
       <p>{count}</p>
       <button onClick={handleIncrease}>+</button>
+      <button onClick={handleDecrease}>-</button>
     </div>
   );
 }
