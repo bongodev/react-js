c6f190e94152234d5b2d2815647824d268c4ad49
kmtusher97 <kmtusher97@gmail.com>
2023-02-17 21:04:06 +0600

    add state manipulation

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 class Counter extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
     console.log('inside constructor', props);
+
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    const previousCount = this.state.count;
+    this.setState({
+      count: previousCount + 1,
+    });
+    console.log('updating state:: ', this.state);
   }
 
   render() {
@@ -14,7 +27,8 @@ class Counter extends React.Component {
       <div>
         <h1>{componentName}</h1>
         <div>
-          <button>+</button>
+          <button onClick={this.increment}>+</button>
+          <p>{this.state.count}</p>
           <button>-</button>
         </div>
       </div>
