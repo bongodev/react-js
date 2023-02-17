ca79e3c12a8decd6b9df4380a47ff26b4329b0dc
kmtusher97 <kmtusher97@gmail.com>
2023-02-17 22:11:38 +0600

    add lifecycle methods

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,6 +6,7 @@ class Counter extends React.Component {
     this.state = {
       dummyState: 'dummy',
       count: 0,
+      user: {},
     };
     console.log('inside constructor', props);
 
@@ -14,19 +15,35 @@ class Counter extends React.Component {
 
   componentDidMount() {
     console.log('executing componentDidMount()....');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data) {
+          return;
+        }
+        this.setState({
+          user: data,
+        });
+      });
   }
 
   componentDidUpdate() {
     console.log('executing componentDidUpdate()....');
     const { count } = this.state;
-    if (count > 10) {
-      alert('too much!!!!');
-    } else if (count < 0) {
-      alert('too low!!!!');
-    }
+    setTimeout(() => {
+      if (count > 10) {
+        alert('too much!!!!');
+      } else if (count < 2) {
+        alert('too low!!!!');
+      }
+    }, 1000);
   }
 
   increment(incrementBy) {
+    if (this.state.count + incrementBy < 0) {
+      alert('sorry!!! can not proceed to nefative');
+      return;
+    }
     this.setState({
       count: this.state.count + incrementBy,
     });
@@ -36,16 +53,18 @@ class Counter extends React.Component {
   render() {
     console.log('rendering.............');
     const { componentName } = this.props;
-    const { count } = this.state;
+    const { count, user } = this.state;
 
     return (
       <div>
+        {console.log('rendered.................')}
         <h1>{componentName}</h1>
         <div>
           <button onClick={() => this.increment(1)}>+</button>
           <p>{count}</p>
           <button onClick={() => this.increment(-1)}>-</button>
         </div>
+        <p>{`Title is ${user.title}`}</p>
       </div>
     );
   }
