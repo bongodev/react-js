502f13153783fef37ebff3280cab8500a43bc9a4
kmtusher97 <kmtusher97@gmail.com>
2023-03-11 21:15:48 +0600

    useEffect dependency

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <Entries />
+      <Counter />
+      {/* <Entries /> */}
     </div>
   );
 }
--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -2,18 +2,28 @@ import { useEffect, useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [clicked, setClicked] = useState(0);
 
   useEffect(() => {
-    console.log('executing useEffect hook');
+    console.log('executing useEffect hook 1');
     document.title = `Current count is ${count}`;
   }, [count]);
 
+  useEffect(() => {
+    console.log('executing useEffect hook 2');
+    if (clicked >= 10 && count >= 10) {
+      alert(`too much clicking ${clicked}`);
+    }
+  }, [clicked, count])
+
   const handleIncrease = () => {
     setCount(count + 1);
+    setClicked((clicked) => clicked + 1);
   };
 
   const handleDecrease = () => {
     setCount(count - 1);
+    setClicked((clicked) => clicked + 1);
   };
 
   console.log('rendering.....');
