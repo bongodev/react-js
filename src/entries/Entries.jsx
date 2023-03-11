5397ca2d3b36e0ee0ff0a218737fd696560d6e4d
kmtusher97 <kmtusher97@gmail.com>
2023-03-11 21:31:43 +0600

    add loading state

--- src/entries/Entries.jsx
+++ src/entries/Entries.jsx
@@ -3,22 +3,30 @@ import { useEffect, useState } from 'react';
 import './Entries.css';
 
 export default function Entries() {
+  const [isLoading, setIsLoading] = useState(false);
   const [entries, setEntries] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const res = await fetch('https://api.publicapis.org/entries');
         const data = await res.json();
         setEntries(data.entries.slice(0, 10));
       } catch (err) {
         alert('failed to load data');
         console.error(err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, []);
+
+  if (isLoading) {
+    return <div>Loading....</div>
+  }
 
   return (
     <div className="etries">
